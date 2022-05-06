71186c051f10a04318ef7793c0b196fc64d04536
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-06 18:50:33 +0300

    templates are created for post creation, update, delete

---
+++ CoLearnSite/.bash_profile
@@ -0,0 +1,2 @@
+export EMAIL_USER="erdemturanlioglu1995@gmail.com"
+export EMAIL_PASS="cecermnvsinzgdat"
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name = 'user-posts'),
     path('about/', views.about, name = 'blog-about'),
-    path('thread/', views.thread, name = "blog-thread")
+    path('feed/', PostListView.as_view(), name = "blog-feed"),
+    path('post/<int:pk>', PostDetailView.as_view(), name = "post-detail"),
+    path('post/new/', PostCreateView.as_view(), name = "post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = "post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = "post-delete")
 ]
