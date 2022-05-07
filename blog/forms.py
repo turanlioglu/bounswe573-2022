6107cdbcf9167384716dd998c24a7f7d5d454f58
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-07 21:45:33 +0300

    forms are created for comments feature

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget = forms.Textarea(attrs = {
+        'class': 'md-textarea form-control',
+        'placeholder': 'Type here...',
+        'rows': '4',
+    }))
+
+    class Meta:
+        model = Comment
+        fields = ['content']
+
