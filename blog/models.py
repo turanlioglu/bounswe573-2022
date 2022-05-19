f521f683fbb2aeb18332b8ed670d0e15520743e6
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-19 18:06:34 +0300

    userprofilepage implementation is started

--- blog/models.py
+++ blog/models.py
@@ -31,10 +31,23 @@ class Comment(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+    image = models.ImageField(default = 'default.jpg', upload_to = 'profile_pics')
     bio = RichTextField()
+    linkedin_url = models.CharField(max_length = 255, null = True, blank = True, )
+
     
     def __str__(self):
         return str(self.user)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size) 
+            img.save(self.image.path)    
 
 class Post(models.Model): 
     course = models.ForeignKey(Course, related_name="post", on_delete=models.CASCADE)
