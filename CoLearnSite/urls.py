bd6f30651ae90fb078d34dc6703492fae888d889
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-19 18:03:59 +0300

    profile is changed to edit profile

--- CoLearnSite/urls.py
+++ CoLearnSite/urls.py
@@ -24,7 +24,7 @@ from courses import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name = 'register'),
-    path('profile/', v.profile, name = 'profile'),
+    path('profile_edit/', v.profile, name = 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name = 'password_reset'),
