724b70abba2bb6cab82b9fd76646a401af73c0b3
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-19 19:50:07 +0300

    view test for blog is applied

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -8,18 +8,11 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.PostListView = reverse('blog-feed')
-        self.PostDetailView = reverse('post-detail', kwargs = {'pk' : 1})
+   
+
 
     def test_PostListView_GET(self):
-        client = Client()
         response = self.client.get(self.PostListView)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/feed.html')
-
-    def test_PostDetailView_GET(self):
-        client = Client()
-        response = self.client.get(self.PostDetailView)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/post_detail.html')
