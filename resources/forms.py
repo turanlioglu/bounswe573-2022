f2dad2b81a82132c62e5f14fbe84366793ce6c6f
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-05-19 11:56:54 +0300

    minor bugs are fixed, before user can see only rooms that they created, but now they can see all the rooms created

--- resources/forms.py
+++ resources/forms.py
@@ -16,6 +16,3 @@ class CreateResourceForm(ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
         super().__init__(*args, **kwargs)
-        user_object = User.objects.filter(username=user.username)
-        new_user_object = get_object_or_404(user_object)
-        self.fields['course'].queryset = self.fields['course'].queryset.filter(user = new_user_object.id)
